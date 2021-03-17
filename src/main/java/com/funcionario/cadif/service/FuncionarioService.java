a6e0e72e25d3c15cab1ecb716fffe4ad2292d64e
Douglas Ribeiro <douglas.ribeiro.id@gmail.com>
2021-03-17 15:31:16 -0300

    vvvvvvvvvvvvvvvvvvvvvvvvvvvv

--- src/main/java/com/funcionario/cadif/service/FuncionarioService.java
+++ src/main/java/com/funcionario/cadif/service/FuncionarioService.java
@@ -73,7 +73,5 @@ public class FuncionarioService {
 		departamento.setDepartamentoId(depto);
 		List<Funcionario> result = funcionarioRepository.findByDepto(departamento); 
 		return result;
-	}
-
-	
+	}	
 }
